b116e7e23f8735b246029029a9f969743aa12b8e
andresprimera <andresprimera@gmail.com>
2026-03-13 22:10:39 -0500

    Backend changes

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:8080',
+      'http://localhost:5173',
+      'http://127.0.0.1:8080',
+      'http://127.0.0.1:5173',
+    ],
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
